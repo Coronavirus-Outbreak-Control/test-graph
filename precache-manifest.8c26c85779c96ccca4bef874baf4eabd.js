0d46e5fd115f710a7cea3fa9c5e98185b35c0ffd
riccio82 <riccio82@gmail.com>
2020-03-30 01:54:44 +0200

    Updates

---
+++ precache-manifest.8c26c85779c96ccca4bef874baf4eabd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8803d081d6e90cc9ec619d97e54973",
+    "url": "/test-graph/index.html"
+  },
+  {
+    "revision": "b1ee9103b171cab779a9",
+    "url": "/test-graph/static/css/2.77b93774.chunk.css"
+  },
+  {
+    "revision": "a2144ba6d6349e108020",
+    "url": "/test-graph/static/css/main.457e0c30.chunk.css"
+  },
+  {
+    "revision": "b1ee9103b171cab779a9",
+    "url": "/test-graph/static/js/2.e9379d0e.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/test-graph/static/js/2.e9379d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2144ba6d6349e108020",
+    "url": "/test-graph/static/js/main.a5599d32.chunk.js"
+  },
+  {
+    "revision": "80e70e083c11236612dd",
+    "url": "/test-graph/static/js/runtime-main.e56cc6de.js"
+  }
+]);
